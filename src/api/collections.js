e0bdb3df093848800295bb12e93ea9f730aa3f8d
Antonio Cerciello <antonio.cerciello@gmail.com>
2021-11-12 09:24:03 +0100

    Changes to collections

--- src/api/collections.js
+++ src/api/collections.js
@@ -8,15 +8,18 @@ class Collections extends Component {
         super(props)
 
         this.state = {
-            url: props.url,
-            collections: []
+            url: props.url,     // url of API
+            collections: [],    // array with collections objects
+            expanded: false       // component expanded
         }
     }
 
+    // Runs on component mount
     componentDidMount() {
+        // Gets collection from API
         axios.get(this.state.url + '/collections?f=json')
-        .then(response => { 
-            
+        .then(response => {
+
             this.setState({
                 collections: response.data.collections
             })
@@ -30,24 +33,44 @@ class Collections extends Component {
         })
     }
 
+    // Runs on component unmount
     componentWillUnmount() {
         this.setState({
                 collections: []
         })
     }
 
+    // Invert expanded state
+    expand = (e) => {
+        this.setState({
+            expanded: !this.state.expanded
+        })
+    }
+
     render() {
-        const{collections} = this.state
+        const{collections, expanded} = this.state
         return(
             <div>
-                <h3>Features collections</h3>
-                <ul>
-                {collections.filter(collection => collection.itemType === 'feature').map(collection => <li key={collection.id}>{collection.title} - {collection.description}</li>)}
-                </ul>
-                <ul>
-                <h3>Other collections</h3>
-                {collections.filter(collection => collection.itemType !== 'feature').map(collection => <li key={collection.id}>{collection.title} - {collection.description}</li>)}
-                </ul>
+                { 
+                    // Collections are loaded but not shown
+                    !expanded && 
+                    <button type="button" onClick={this.expand}>Show {collections.length} features</button> 
+                }
+                { 
+                    // Collections are shown
+                    expanded && 
+                    <div>
+                        <h3>Features collections</h3>
+                        <ul>
+                            {collections.filter(collection => collection.itemType === 'feature').map(collection => <li key={collection.id}>{collection.title} - {collection.description}</li>)}
+                        </ul>
+                        <ul>
+                            <h3>Other collections</h3>
+                            {collections.filter(collection => collection.itemType !== 'feature').map(collection => <li key={collection.id}>{collection.title} - {collection.description}</li>)}
+                        </ul>
+                        <button type="button" onClick={this.expand}>Hide</button>
+                    </div>
+                }
             </div>
             )
     }
